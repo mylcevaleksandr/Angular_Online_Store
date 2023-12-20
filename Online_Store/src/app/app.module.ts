95b6c45adfb6dfa7a7d4ae44c896074820168b09
mylcevaleksandr <myltsevaleksandr@gmail.com>
2023-12-20 15:56:51 +0300

    Header markup finished

--- Online_Store/src/app/app.module.ts
+++ Online_Store/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LayoutComponent } from './shared/layout/layout.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { MainComponent } from './views/main/main.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -16,10 +17,11 @@ import { MainComponent } from './views/main/main.component';
     FooterComponent,
     MainComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Online_Store/src/app/shared/layout/header/header.component.ts
+++ Online_Store/src/app/shared/layout/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss','../../../../assets/styles/_variables.scss']
 })
 export class HeaderComponent {
 
