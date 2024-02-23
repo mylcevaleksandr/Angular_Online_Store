c3ffc3605ddb2a051911f2cf288c409083b9e5f1
mylcevaleksandr <myltsevaleksandr@gmail.com>
2024-02-23 04:18:59 +0300

    Fixed routes / in invironments.dev

--- Online_Store/src/environments/environment.ts
+++ Online_Store/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://websitecreator.dev//api/',
-  serverStaticPath: 'https://websitecreator.dev//images/products/'
+  apiUrl: 'https://websitecreator.dev/api/',
+  serverStaticPath: 'https://websitecreator.dev/images/products/'
 };
